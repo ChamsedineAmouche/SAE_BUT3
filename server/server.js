d7780842c2371a19863e6b77d7d2bd32cb95ef0b
abdel <abdelnour.rekkab@gmail.com>
2024-11-22 20:06:03 +0100

    wip

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const { fetchData, fetchData2 } = require('./read/homepageFetcher');
+
+app.get('/', (req, res) => {
+    console.log("Endpoint '/' was called");
+    res.send('Hello, World!');
+});
 
 app.get("/api", (req, res) => {
-    res.json({"test" : ["test1", "test2"]})
+    console.log("Endpoint '/api' was called");
+    const data =fetchData()
+    res.json(data);
+})
+
+app.get("/api1", (req, res) => {
+    console.log("Endpoint '/api1' was called");
+    const data1 = fetchData2()
+    res.json(data1)
 })
 
 app.listen(5000, () =>{console.log("Server started on port 5000")})
