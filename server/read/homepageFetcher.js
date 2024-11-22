55e1f94ac08f49f119c26e471f18b4100951b24e
abdel <abdelnour.rekkab@gmail.com>
2024-11-22 20:09:38 +0100

    wip

---
+++ server/read/homepageFetcher.js
@@ -0,0 +1,9 @@
+function fetchData() {
+    return { test: ["caca", "zmig", "wooooooohhouuu"] };
+}
+
+function fetchData2() {
+    return {test1: ["je suis", "juste un", "chill guy"]}
+}
+
+module.exports = {fetchData, fetchData2};
