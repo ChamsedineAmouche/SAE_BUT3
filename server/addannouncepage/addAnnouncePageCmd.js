d22db1468dc1cfe60472cdcfd6f3e9bc96240e61
Noah Guerreiro <noah.guerreiro1801@gmail.com>
2024-11-23 15:51:04 +0100

    K-32 : refacto de db functions

--- server/addannouncepage/addAnnouncePageCmd.js
+++ server/addannouncepage/addAnnouncePageCmd.js
@@ -1,13 +1,11 @@
-const getDbConnection = require('../db_utils/db_connection');
-const execute_query = require('../db_utils/db_functions');
+const {getResultOfQuery} = require('../db_utils/db_functions');
 
 const mysql = require('mysql2');
 
 async function getCategoriesForObjects() {
     try {
-        const connection = getDbConnection('vue_user');
-        const query = 'SELECT label FROM object_type';
-        const result = await execute_query(connection, query);
+        const result =
+            await getResultOfQuery('vue_user', 'SELECT label FROM object_type');
         console.log('Résultats de la requête :', result);
         return result;
     } catch (error) {
@@ -17,16 +15,16 @@ async function getCategoriesForObjects() {
 
 async function getLocalisationOfStockage() {
     try {
-        const connection = getDbConnection('vue_user');
-        const query = 'SELECT adress, city, zipcode, capacity FROM container';
-        const result = await execute_query(connection, query);
+        const result =
+            await getResultOfQuery('vue_user',
+                'SELECT adress, city, zipcode, capacity FROM container');
         const containerAvailable = result.filter(container => container.capacity > 0);
         console.log('Résultats de la requête :', result);
-        return result;
+        return containerAvailable;
     } catch (error) {
         console.error('Erreur lors de la récupération des données :', error);
     }
 }
 
 
-module.exports = { getCategoriesForObjects, getLocalisationOfStockage };
+module.exports = {getCategoriesForObjects, getLocalisationOfStockage};
--- server/homepage/homepageFetcher.js
+++ server/homepage/homepageFetcher.js
@@ -1,13 +1,11 @@
-const getDbConnection = require('../db_utils/db_connection');
-const execute_query = require('../db_utils/db_functions');
+const {getResultOfQuery} = require('../db_utils/db_functions');
 
 const mysql = require('mysql2');
 
 async function getNumberOfCompany() {
     try {
-        const connection = getDbConnection('vue_user');
-        const query = 'SELECT COUNT(*) AS count FROM company';
-        const result = await execute_query(connection, query);
+        const result =
+            await getResultOfQuery('vue_user', 'SELECT COUNT(*) AS count FROM company');
         const count = result[0]?.count;
         console.log('Résultats de la requête :', result);
         return count;
