77c9fe053e6c80a6aadec1b649ea0a4a42001e9c
Noah Guerreiro <noah.guerreiro1801@gmail.com>
2024-11-23 14:38:58 +0100

    K-32 : renvoie des categories d'objet dans le mdd

---
+++ server/addannouncepage/addAnnouncePageCmd.js
@@ -0,0 +1,19 @@
+const getDbConnection = require('../db_utils/db_connection');
+const execute_query = require('../db_utils/db_functions');
+
+const mysql = require('mysql2');
+
+async function getCategoriesForObjects() {
+    try {
+        const connection = getDbConnection('vue_user');
+        const query = 'SELECT label FROM object_type';
+        const result = await execute_query(connection, query);
+        console.log('Résultats de la requête :', result);
+        return result;
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données :', error);
+    }
+}
+
+
+module.exports = { getCategoriesForObjects };
