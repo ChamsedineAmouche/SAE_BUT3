e50e5331fb0c251b9ebc0cd5d2d3e82bbe8dd58f
abdelrkb <144436695+abdelrkb@users.noreply.github.com>
2024-11-25 22:21:27 +0000

    wip

--- server/db_utils/db_connection.js
+++ server/db_utils/db_connection.js
@@ -1,1 +1,11 @@
 //A remplir avec le template de db_connection
+module.exports = {
+    connect: () => {
+        console.log('Default connect function called.');
+        return {};  // Retourne une fausse connexion
+    },
+    query: (sql, params) => {
+        console.log(`Default query function called with SQL: ${sql}`);
+        return Promise.resolve([]);  // Simule une réponse vide
+    },
+};
