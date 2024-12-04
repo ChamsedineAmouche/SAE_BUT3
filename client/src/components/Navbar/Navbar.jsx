a745c4300c8550d111165a1a3a6ee34b19c375fb
abdelrkb <abdelnour.rekkab@gmail.com>
2024-12-05 00:15:39 +0100

    wip

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -60,19 +60,19 @@ const Navbar = () => {
 
         {/* Search bar */}
         <div className="hidden md:flex items-center relative w-72 mr-12">
-          <button className="bg-darkGreen text-white px-4 py-2 rounded-l-full">
-            <FontAwesomeIcon icon={faEllipsis} />
-          </button>
-          <input
-            type="text"
-            placeholder="Rechercher"
-            className="bg-white text-black px-4 py-2 rounded-md focus:outline-none w-full rounded-r-full"
-          />
-          <FontAwesomeIcon
-            icon={faSearch}
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-          />
-        </div>
+            <button className="bg-darkGreen text-white px-4 py-2 rounded-l-full">
+              <FontAwesomeIcon icon={faEllipsis} />
+            </button>
+            <input
+              type="text"
+              placeholder="Rechercher"
+              className="bg-white text-black px-4 py-2 rounded-md focus:outline-none w-full rounded-r-full"
+            />
+            <FontAwesomeIcon
+              icon={faSearch}
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+            />
+          </div>
 
         {/* Right Section - Search bar and Icons */}
         <div className="flex items-center flex-shrink-0 space-x-4">
