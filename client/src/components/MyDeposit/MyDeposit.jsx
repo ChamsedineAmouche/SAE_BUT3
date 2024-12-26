58dbea4c4bd2065f632021bfdab99af12aad84c0
elyasmlm <elyas@CSM.>
2024-12-26 20:34:04 +0100

    page dépots de l'user

---
+++ client/src/components/MyDeposit/MyDeposit.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Switch from "../Switch/Switch";
+import SquareGrid from "../SquareGrid/SquareGrid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+
+const MyDeposit = () => {
+  const [selectedOption, setSelectedOption] = useState("Dépots");
+
+  const handleSwitchChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="bg-white w-full">
+      <button className="bg-oliveGreen text-white px-6 py-3 text-lg font-semibold rounded-md hover:bg-opacity-90 transition duration-200 absolute top-24 right-3">
+        <FontAwesomeIcon icon={faPlusSquare} className="pr-2"/>
+        Dépôts
+      </button>
+      <h2 className="text-3xl font-bold text-darkGreen mb-8 text-center">Mes Dépots</h2>
+      
+      <Switch 
+        option1Title={"Publié"} 
+        option2Title={"Brouillon"} 
+        selectedDefault={"Publié"} 
+        onSwitchChange={handleSwitchChange}
+      />
+
+      {selectedOption === "Publié" && (
+        <div id="Publié" className="overflow-y-auto h-[70vh]">
+          <SquareGrid items={['exemple', 'exemple', 'exemple', 'exemple', 'exemple', 'exemple', 'exemple', 'exemple']}/>
+        </div>
+      )}
+      {selectedOption === "Brouillon" && (
+        <div id="Brouillon" className="overflow-y-auto h-[70vh]">
+          <SquareGrid items={['exempddle', 'exemple', 'exemple', 'exemple', 'exemple', 'exemple', 'exemple', 'exemple']}/>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyDeposit;
--- client/src/components/Switch/Switch.jsx
+++ client/src/components/Switch/Switch.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Switch = ({ option1Title, option2Title }) => {
+const Switch = ({ option1Title, option2Title, selectedDefault, onSwitchChange }) => {
+  const [selected, setSelected] = useState(selectedDefault);
+
+  const handleClick = (option) => {
+    setSelected(option);
+    if (onSwitchChange) {
+      onSwitchChange(option);
+    }
+  };
+
   return (
-    <div className="bg-yellowGreen1 bg-opacity-10 mx-12 flex space-x-4 p-2 items-center justify-center rounded-full">
-      <span className="bg-white text-darkGreen p-2 rounded-full text-lg font-semibold">{option1Title}</span>
-      <span className="bg-white text-darkGreen p-2 rounded-full text-lg font-semibold">{option2Title}</span>
+    <div className="flex items-center justify-center">
+      <div className="bg-yellowGreen1 bg-opacity-10 mx-12 flex space-x-4 p-2 items-center justify-center rounded-full">
+        <span
+          onClick={() => handleClick(option1Title)}
+          className={`py-2 px-4 rounded-full text-lg font-semibold w-44 text-center cursor-pointer ${
+            selected === option1Title ? "bg-darkGreen text-white" : "bg-white text-darkGreen"
+          }`}
+        >
+          {option1Title}
+        </span>
+        <span
+          onClick={() => handleClick(option2Title)}
+          className={`py-2 px-4 rounded-full text-lg font-semibold w-44 text-center cursor-pointer ${
+            selected === option2Title ? "bg-darkGreen text-white" : "bg-white text-darkGreen"
+          }`}
+        >
+          {option2Title}
+        </span>
+      </div>
     </div>
   );
 };
