afeaa0c9f2a1fc6ff3b5ee62536a2683587adede
LaxhP <laxhan.push@gmail.com>
2025-02-13 14:44:30 +0100

    politique

---
+++ client/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold text-darkGreen-700 mb-6">Politique de confidentialité - Green Circle</h1>
+
+      <section className="mt-6">
+        <h2 className="text-2xl font-semibold text-darkGreen-700">ARTICLE 1 : PRÉAMBULE</h2>
+        <p className="text-gray-700 mt-2">
+          La présente politique de confidentialité a pour but d’informer les utilisateurs du site :
+        </p>
+        <ul className="list-disc pl-6 text-gray-700">
+          <li>Sur la manière dont sont collectées leurs données personnelles.</li>
+          <li>Sont considérées comme des données personnelles, toute information permettant d’identifier un utilisateur (ex : noms, prénoms, âge, adresse postale ou email, localisation, adresse IP, etc.).</li>
+          <li>Sur les droits dont ils disposent concernant ces données.</li>
+          <li>Sur la personne responsable du traitement des données à caractère personnel collectées et traitées.</li>
+          <li>Sur les destinataires de ces données personnelles.</li>
+          <li>Sur la politique du site en matière de cookies.</li>
+        </ul>
+      </section>
+
+      <section className="mt-6">
+        <h2 className="text-2xl font-semibold text-darkGreen-700">ARTICLE 2 : PRINCIPES RELATIFS À LA COLLECTE ET AU TRAITEMENT DES DONNÉES PERSONNELLES</h2>
+        <ul className="list-disc pl-6 text-gray-700">
+          <li>Traitées de manière licite, loyale et transparente.</li>
+          <li>Collectées pour des finalités déterminées et légitimes.</li>
+          <li>Adéquates, pertinentes et limitées à ce qui est nécessaire.</li>
+          <li>Exactes et, si nécessaire, tenues à jour.</li>
+          <li>Conservées sous une forme permettant l’identification des personnes concernées.</li>
+          <li>Traitées de façon à garantir une sécurité appropriée.</li>
+        </ul>
+      </section>
+
+      <section className="mt-6">
+        <h2 className="text-2xl font-semibold text-darkGreen-700">ARTICLE 3 : DONNÉES À CARACTÈRE PERSONNEL COLLECTÉES ET TRAITÉES DANS LE CADRE DE LA NAVIGATION SUR LE SITE</h2>
+
+        <h3 className="text-xl font-semibold text-darkGreen-700 mt-4">Article 3.1 : Données collectées</h3>
+        <p className="text-gray-700 mt-2">Les données personnelles collectées dans le cadre de notre activité sont les suivantes :</p>
+        <ul className="list-disc pl-6 text-gray-700">
+          <li>Informations sur l’entreprise : adresse, numéro de SIRET</li>
+          <li>Informations de contact : nom, prénom, e-mail</li>
+        </ul>
+        <p className="text-gray-700 mt-2">La collecte et le traitement de ces données répond aux finalités suivantes :</p>
+        <ul className="list-disc pl-6 text-gray-700">
+          <li>Publier et gérer les annonces d'offre de matériel</li>
+          <li>Mettre en relation les entreprises donneuses et récupératrices</li>
+          <li>Garantir le bon fonctionnement du service</li>
+        </ul>
+
+        <h3 className="text-xl font-semibold text-darkGreen-700 mt-4">Article 3.2 : Mode de collecte des données</h3>
+        <p className="text-gray-700 mt-2">Lorsque vous utilisez notre site, aucune donnée n'est collecté automatiquement.</p>
+        <p className="text-gray-700 mt-2">D’autres données personnelles sont collectées lors de l’inscription sur le site :</p>
+        <ul className="list-disc pl-6 text-gray-700">
+          <li>Informations sur l’entreprise : adresse, numéro de SIRET</li>
+          <li>Informations de contact : nom, prénom, e-mail</li>
+        </ul>
+        <p className="text-gray-700 mt-2">Elles sont conservées par le responsable du traitement dans des conditions raisonnables de sécurité, pour une durée de : 1 an. La société est susceptible de conserver certaines données à caractère personnel au-delà des délais annoncés afin de remplir ses obligations légales ou réglementaires.</p>
+
+
+
+        <h3 className="text-xl font-semibold text-darkGreen-700 mt-4">Article 3.3 : Hébergement des données</h3>
+        <p className="text-gray-700 mt-2">Le site GreenCircle est hébergé par :</p>
+        <p className="text-gray-700 font-semibold">Netinfosmédias, sur les serveurs de la société OVH, dont le siège est situé à l’adresse ci-après :</p>
+        <p className="text-gray-700 font-semibold">2 rue Kellermann, 59100 Roubaix, France</p>
+        <p className="text-gray-700 font-semibold">Numéro de téléphone : 03 22 66 12 10</p>
+
+        <h3 className="text-xl font-semibold text-darkGreen-700 mt-4">Article 3.4 : Politique en matière de cookies</h3>
+        <p className="text-gray-700 mt-2">
+          Les cookies utilisés sur notre site sont strictement nécessaires au bon fonctionnement du service. Ils permettent notamment de gérer l’authentification des utilisateurs et de garantir la continuité de la session utilisateur. Aucun cookie n'est utilisé à des fins publicitaires ou de suivi marketing.
+        </p>
+      </section>
+
+      <section className="mt-6">
+        <h2 className="text-2xl font-semibold text-darkGreen-700">ARTICLE 4 : RESPONSABLE DU TRAITEMENT DES DONNÉES ET DÉLÉGUÉ À LA PROTECTION DES DONNÉES</h2>
+        <h3 className="text-xl font-semibold text-darkGreen-700 mt-4">Article 4.1 : Le responsable du traitement des données</h3>
+        <p className="text-gray-700 mt-2">Les données à caractère personnel sont collectées par <span className="font-semibold">GreenCircle</span>.</p>
+        <p className="text-gray-700 mt-2">Le responsable du traitement des données à caractère personnel peut être contacté de la manière suivante :</p>
+        <p className="text-gray-700 font-semibold">Par courrier :</p>
+        <p className="text-gray-700">Val d’Europe agglomération – Château de Chessy – BP 40 – 77701 Marne-la-Vallée cedex 4</p>
+        <p className="text-gray-700 font-semibold">Par mail :</p>
+        <p className="text-gray-700">admin@greencircle.fr</p>
+
+
+        <h3 className="text-xl font-semibold text-darkGreen-700 mt-4">Article 4.2 : Le délégué à la protection des données</h3>
+<p className="text-gray-700 mt-2">Le délégué à la protection des données de l’entreprise ou du responsable est <span className="font-semibold">M. Dpo Green</span>, joignable au mail :</p>
+<p className="text-gray-700 font-semibold">Par mail :</p>
+<p className="text-gray-700">dpo@greencircle.fr</p>
+<p className="text-gray-700 mt-2">
+  Si vous estimez, après nous avoir contactés, que vos droits “Informatique et Libertés” ne sont pas respectés, 
+  vous pouvez adresser une information à la <span className="font-semibold">CNIL</span>.
+</p>
+      </section>
+
+      <section className="mt-6">
+      <h2 className="text-2xl font-semibold text-darkGreen-700 mt-6">
+  ARTICLE 5 : LES DROITS DE L’UTILISATEUR EN MATIÈRE DE COLLECTE ET DE TRAITEMENT DES DONNÉES
+</h2>
+<p className="text-gray-700 mt-2">
+  Tout utilisateur concerné par le traitement de ses données personnelles peut se prévaloir des droits suivants, 
+  en application du règlement européen 2016/679 et de la Loi Informatique et Liberté (Loi 78-17 du 6 janvier 1978) :
+</p>
+<ul className="list-disc pl-6 text-gray-700 mt-2">
+  <li>Droit d’accès, de rectification et droit à l’effacement des données (articles 15, 16 et 17 du RGPD).</li>
+  <li>Droit à la portabilité des données (article 20 du RGPD).</li>
+  <li>Droit à la limitation (article 18 du RGPD) et à l’opposition du traitement des données (article 21 du RGPD).</li>
+  <li>Droit de ne pas faire l’objet d’une décision fondée exclusivement sur un procédé automatisé.</li>
+  <li>Droit de déterminer le sort des données après la mort.</li>
+  <li>Droit de saisir l’autorité de contrôle compétente (article 77 du RGPD).</li>
+</ul>
+<p className="text-gray-700 mt-2">
+  L’utilisateur a le droit d’accéder à ses données personnelles ou demander la suppression de son compte directement depuis la section 
+  <span className="font-semibold"> "Paramètres" </span> de la page <span className="font-semibold"> "Mon compte" </span> sur notre site.
+</p>
+<p className="text-gray-700 mt-2">
+  L’utilisateur peut également envoyer un email pour exercer ses droits à l’adresse :
+</p>
+<p className="text-gray-700 font-semibold">communication@vdeagglo.fr</p>
+<p className="text-gray-700 mt-2">Ou par courrier à l’adresse :</p>
+<p className="text-gray-700 font-semibold">
+  Val d’Europe agglomération <br />
+  Château de Chessy – BP 40 <br />
+  77701 Marne-la-Vallée cedex 4
+</p>
+<p className="text-gray-700 mt-2">
+  Afin que le responsable du traitement des données puisse faire droit à sa demande, l’utilisateur peut être tenu de lui communiquer certaines informations 
+  telles que : ses noms et prénoms, son adresse e-mail ainsi que son numéro de compte, d’espace personnel ou d’abonné.
+</p>
+<p className="text-gray-700 mt-2">
+  Consultez le site <a href="https://www.cnil.fr" className="text-blue-600 underline">cnil.fr</a> pour plus d’informations sur vos droits.
+</p>
+      </section>
+
+      <section className="mt-6">
+      <h2 className="text-2xl font-semibold text-darkGreen-700 mt-6">
+  ARTICLE 6 : CONDITIONS DE MODIFICATION DE LA POLITIQUE DE CONFIDENTIALITÉ
+</h2>
+<p className="text-gray-700 mt-2">
+  L’éditeur du site <span className="font-semibold">GreenCircle</span> se réserve le droit de modifier la présente Politique à tout moment 
+  afin d’assurer aux utilisateurs du site sa conformité avec le droit en vigueur.
+</p>
+<p className="text-gray-700 mt-2">
+  Les éventuelles modifications ne sauraient avoir d’incidence sur les achats antérieurement effectués sur le site, 
+  lesquels restent soumis à la Politique en vigueur au moment de l’achat et telle qu’acceptée par l’utilisateur lors de la validation de l’achat.
+</p>
+<p className="text-gray-700 mt-2">
+  L’utilisateur est invité à prendre connaissance de cette Politique à chaque fois qu’il utilise nos services, sans qu’il soit nécessaire de l’en prévenir formellement.
+</p>
+<p className="text-gray-700 mt-2">
+  La présente politique, éditée le <span className="font-semibold">12/02/25</span>, a été mise à jour le <span className="font-semibold">12/02/25</span>.
+</p>
+
+      </section>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
